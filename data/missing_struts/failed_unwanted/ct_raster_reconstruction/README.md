8990e21b327a2dc52bd1450194d20a3ad6ae85f6
Danny Villanueva <dannee104@gmail.com>
2026-07-21 16:14:41 -0700

    Register Brian Tran CAD model to CT

---
+++ data/missing_struts/failed_unwanted/ct_raster_reconstruction/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c3b7b78297380568775bd0670853915385902fde6fc4b6a5e73b81a2adafba4
+size 1519
---
+++ data/missing_struts/failed_unwanted/ct_raster_reconstruction/reconstruct_registered_ct_stl.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d3ec9731a45e4926bc4fd74a9698a8065f0f7db247788425fca025bf6d6fb8f
+size 13420
